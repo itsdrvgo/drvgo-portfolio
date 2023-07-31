5fb132e69ea2863155edb02dafb2a220888260e2
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-07-31 17:23:40 +0530

    changed profile skeleton

--- src/components/skeletons/profile-skeleton.tsx
+++ src/components/skeletons/profile-skeleton.tsx
@@ -11,18 +11,48 @@ import { Skeleton } from "../ui/skeleton";
 
 function ProfileSkeleton({ className }: DefaultProps) {
     return (
-        <Card className="bg-background">
-            <CardHeader>
-                <CardTitle>Username</CardTitle>
-                <CardDescription>Change your username</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Skeleton className="h-11 w-2/5" />
-            </CardContent>
-            <CardFooter>
-                <Skeleton className="h-10 w-24" />
-            </CardFooter>
-        </Card>
+        <>
+            <Card className="bg-background">
+                <CardHeader>
+                    <CardTitle>Profile Picture</CardTitle>
+                    <CardDescription>
+                        Change your profile picture
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="flex items-center gap-14">
+                    <Skeleton className="h-40 w-40 rounded-full" />
+                    <Skeleton className="h-10 w-36" />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-10 w-24" />
+                </CardFooter>
+            </Card>
+
+            <Card className="bg-background">
+                <CardHeader>
+                    <CardTitle>Username</CardTitle>
+                    <CardDescription>Change your username</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Skeleton className="h-11 w-2/5" />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-10 w-24" />
+                </CardFooter>
+            </Card>
+
+            <Card className="bg-background">
+                <CardHeader>
+                    <CardTitle>Danger Zone</CardTitle>
+                    <CardDescription>
+                        Delete your account permanantly
+                    </CardDescription>
+                </CardHeader>
+                <CardFooter className="flex justify-end">
+                    <Skeleton className="h-10 w-36" />
+                </CardFooter>
+            </Card>
+        </>
     );
 }
 
