4f57feaf322fae48d63fff7a7d034baa97f9fc25
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-08-03 20:33:12 +0530

    og -trial 01

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,13 @@ export const metadata: Metadata = {
         title: siteConfig.name,
         description: siteConfig.description,
         siteName: siteConfig.name,
-        images: [`${siteConfig.url}/og.png`],
+        images: [
+            {
+                url: `${siteConfig.url}/og.png`,
+                width: 1200,
+                height: 630,
+            },
+        ],
     },
     twitter: {
         card: "summary_large_image",
