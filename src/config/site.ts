088582c6fe35ba420745117f2c74c6b74a404f8e
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-08-03 20:10:24 +0530

    fixed site url

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,8 +2,8 @@ export const siteConfig = {
     name: "DRVGO",
     description:
         "Discover the creative world of DRVGO Portfolio - a harmonious fusion of art and technology. 🚀 Explore my journey and get inspired by the possibilities of creating impactful digital experiences. 🎨 Let's connect and embark on this exciting tech adventure together!",
-    url: "http://itsdrvgo.vercel.app",
-    ogImage: "http://itsdrvgo.vercel.app/og.jpg",
+    url: "https://itsdrvgo.vercel.app",
+    ogImage: "https://itsdrvgo.vercel.app/og.jpg",
     keywords: [
         "DRVGO",
         "drago",
