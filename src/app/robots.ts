d25b8e2c82537b2a250a5137ba8205021c84260d
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-08-05 17:27:08 +0530

    updated robots to disallow admins

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
+            disallow: "/admin/",
         },
     };
 }
