40c327e73dafee92564dbe7b244f95c6586366dd
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-08-15 14:20:03 +0530

    added endpoint to get comments directly

--- src/app/api/blogs/comments/[blogId]/route.ts
+++ src/app/api/blogs/comments/[blogId]/route.ts
@@ -3,8 +3,38 @@ import { comments, insertCommentSchema, users } from "@/src/lib/drizzle/schema";
 import { handleError } from "@/src/lib/utils";
 import { BlogContext, blogContextSchema } from "@/src/lib/validation/route";
 import { currentUser } from "@clerk/nextjs";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, context: BlogContext) {
+    try {
+        const authUser = await currentUser();
+        if (!authUser)
+            return NextResponse.json({
+                code: 403,
+                message: "Unauthorized!",
+            });
+
+        const { params } = blogContextSchema.parse(context);
+
+        const filteredComments = await db.query.comments.findMany({
+            where: eq(comments.blogId, Number(params.blogId)),
+            with: {
+                user: true,
+                loves: true,
+            },
+            orderBy: [desc(comments.createdAt)],
+        });
+
+        return NextResponse.json({
+            code: 200,
+            message: "Ok",
+            data: JSON.stringify(filteredComments),
+        });
+    } catch (err) {
+        handleError(err);
+    }
+}
 
 export async function POST(req: NextRequest, context: BlogContext) {
     const body = await req.json();
