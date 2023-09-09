f0f0312f37301a6b018f0cf30570f881f6ad987f
Sarthak Kundu <thedragoluca@gmail.com>
2023-09-09 21:24:25 +0530

    bug fix for providers

--- src/components/providers/server.tsx
+++ src/components/providers/server.tsx
@@ -1,15 +1,8 @@
-"use client";
-
 import { DefaultProps } from "@/src/types";
 import { ClerkProvider } from "@clerk/nextjs";
-import { NextUIProvider } from "@nextui-org/react";
 
 function ServerProvider({ children }: DefaultProps) {
-    return (
-        <NextUIProvider>
-            <ClerkProvider>{children}</ClerkProvider>
-        </NextUIProvider>
-    );
+    return <ClerkProvider>{children}</ClerkProvider>;
 }
 
 export default ServerProvider;
