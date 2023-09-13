ee15810f749a2896ac04a3bb7e25318ff965beb4
Sarthak Kundu <thedragoluca@gmail.com>
2023-09-13 23:57:42 +0530

    updated ui for users table

--- src/components/admin/users/users-view.tsx
+++ src/components/admin/users/users-view.tsx
@@ -1,11 +1,34 @@
 import { db } from "@/src/lib/drizzle";
+import { users } from "@/src/lib/drizzle/schema";
+import { userSchema } from "@/src/lib/validation/user";
 import { DefaultProps } from "@/src/types";
-import UserTable from "./users-table";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import UserTable, { PartialUser } from "./users-table";
 
 async function UsersView({ className }: DefaultProps) {
-    const users = await db.query.users.findMany();
+    const [user, allUsers] = await Promise.all([
+        currentUser(),
+        db
+            .select({
+                id: users.id,
+                username: users.username,
+                email: users.email,
+                role: users.role,
+                created: users.createdAt,
+                image: users.image,
+            })
+            .from(users),
+    ]);
 
-    return <UserTable className={className} data={users} />;
+    if (!user) return redirect("/auth");
+    const parsedUser = userSchema.parse(user);
+
+    const data = allUsers as unknown as PartialUser[];
+
+    return (
+        <UserTable className={className} data={data} authUser={parsedUser} />
+    );
 }
 
 export default UsersView;
