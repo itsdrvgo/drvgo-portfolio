b4140c423bc06b1c26a9e668dbc057e7e628cf71
Sarthak Kundu <thedragoluca@gmail.com>
2023-11-04 13:23:38 +0530

    updated nextui provider

--- src/components/providers/client.tsx
+++ src/components/providers/client.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { DefaultProps } from "@/src/types";
-import { NextUIProvider } from "@nextui-org/system";
+import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useRouter } from "next/navigation";
 
 const queryClient = new QueryClient();
 
 function ClientProvider({ children }: DefaultProps) {
+    const router = useRouter();
+
     return (
-        <NextUIProvider>
+        <NextUIProvider navigate={router.push}>
             <QueryClientProvider client={queryClient}>
                 {children}
                 <ReactQueryDevtools initialIsOpen={true} />
