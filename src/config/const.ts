2dfd3590c0c7ad0fa64572e9351142f703c377f5
Sarthak Kundu <thedragoluca@gmail.com>
2024-02-01 00:04:42 +0530

    Add image extensions to const.ts

--- src/config/const.ts
+++ src/config/const.ts
@@ -2,3 +2,4 @@ import BlogThumbnail from "@/public/pattern.webp";
 
 export const DEFAULT_BLOG_THUMBNAIL = BlogThumbnail;
 export const GITHUB_BASE_URL = "https://github.com/";
+export const IMAGE_EXTENSIONS = ["png", "jpg", "jpeg", "gif", "webp"];
