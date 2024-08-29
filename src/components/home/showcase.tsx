47510ebbaf091f05b4cef1d26646fe2bcde86cb2
itsdrvgo <thedragoluca@gmail.com>
2024-08-29 23:04:38 +0530

    ADD: added new cryptor to showcase

--- src/components/home/showcase.tsx
+++ src/components/home/showcase.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CreateEV0App from "@/../public/projects/create_ev0_app.jpeg";
+import Cryptor from "@/../public/projects/cryptor.jpeg";
 import PeerAmpFS from "@/../public/projects/peeramp_fs.jpeg";
 import PostItBG from "@/../public/projects/post_it.jpeg";
 import PrimeBot from "@/../public/projects/prime_bot.jpeg";
@@ -19,6 +20,7 @@ interface Project {
     source: string;
     demo?: string;
     thumbnail: StaticImageData;
+    isActive?: boolean;
 }
 
 const projects: Project[] = [
@@ -29,18 +31,30 @@ const projects: Project[] = [
         source: GITHUB_BASE_URL + "itsdrvgo/post-it",
         demo: "https://post-it-itsdrvgo.vercel.app/",
         thumbnail: PostItBG,
+        isActive: true,
     },
     {
         name: "Routez",
         description: "Next.JS like file-based routing for Express.js",
         source: GITHUB_BASE_URL + "itsdrvgo/routez",
         thumbnail: Routez,
+        isActive: true,
     },
     {
         name: "PeerAmp File Server",
         description: "The official server of PeerAmp to handle files",
         source: GITHUB_BASE_URL + "itsdrvgo/peeramp-file-server",
         thumbnail: PeerAmpFS,
+        isActive: true,
+    },
+    {
+        name: "Cryptor",
+        description:
+            "A simple, secure, and fast encryption and decryption tool",
+        source: GITHUB_BASE_URL + "itsdrvgo/cryptor",
+        demo: "https://cryptor-itsdrvgo.vercel.app/",
+        thumbnail: Cryptor,
+        isActive: true,
     },
     {
         name: "Create EV0 App",
@@ -49,6 +63,7 @@ const projects: Project[] = [
         source: GITHUB_BASE_URL + "itsdrvgo/create-ev0-app",
         demo: "https://ev0.vercel.app/",
         thumbnail: CreateEV0App,
+        isActive: true,
     },
     {
         name: "XAR OS",
@@ -57,6 +72,7 @@ const projects: Project[] = [
         source: GITHUB_BASE_URL + "itsdrvgo/xar-os",
         demo: "https://xar-os.vercel.app/",
         thumbnail: XarOSBG,
+        isActive: true,
     },
     {
         name: "Prime (Discord Bot)",
@@ -79,45 +95,50 @@ export function Showcase({ className, ...props }: GenericProps) {
             }}
             {...props}
         >
-            <div className="flex w-full max-w-7xl flex-col-reverse gap-10 md:flex-row md:gap-24">
+            <div className="flex w-full max-w-7xl flex-col-reverse items-center gap-10 md:flex-row md:gap-24">
                 <div className="grid w-full gap-5 md:grid-cols-3 md:gap-10">
-                    {projects.map((project, i) => (
-                        <div
-                            className={cn(
-                                "group relative h-52 w-full overflow-hidden rounded-2xl border border-foreground/10 shadow-lg drop-shadow-lg",
-                                (i === 0 || i == 3 || i == 4) && "md:col-span-2"
-                            )}
-                            key={i}
-                        >
-                            <Image
-                                src={project.thumbnail}
-                                alt={project.name}
-                                className="size-full object-cover"
-                            />
+                    {projects
+                        .filter((project) => project.isActive)
+                        .map((project, i) => (
+                            <div
+                                className={cn(
+                                    "group relative h-52 w-full overflow-hidden rounded-2xl border border-foreground/10 shadow-lg drop-shadow-lg",
+                                    (i === 0 || i == 3 || i == 4) &&
+                                        "md:col-span-2",
+                                    projects.length % 2 !== 0 &&
+                                        i === projects.length - 1 &&
+                                        "md:col-span-3"
+                                )}
+                                key={i}
+                            >
+                                <Image
+                                    src={project.thumbnail}
+                                    alt={project.name}
+                                    className="size-full object-cover"
+                                />
 
-                            <div className="absolute left-0 top-0 flex size-full flex-col justify-end gap-2 text-balance bg-foreground/40 p-5 text-background transition-all ease-in-out group-hover:bg-foreground/80">
-                                <div className="-translate-y-full space-y-1 opacity-0 transition-all ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
-                                    <Link
-                                        href={project.demo ?? project.source}
-                                        className="group/link flex w-fit items-center gap-1 font-medium hover:underline"
-                                        target="_blank"
-                                    >
-                                        {project.demo
-                                            ? "View Demo"
-                                            : "View Source"}
-
-                                        <Icons.chevronRight className="size-0 transition-all ease-in-out group-hover/link:size-5" />
-                                    </Link>
-
-                                    <p className="text-background/60">
-                                        {project.description}
-                                    </p>
+                                <div className="absolute left-0 top-0 flex size-full flex-col justify-end gap-2 text-balance bg-foreground/40 p-5 text-background transition-all ease-in-out group-hover:bg-foreground/80">
+                                    <div className="-translate-y-full space-y-1 opacity-0 transition-all ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
+                                        <Link
+                                            href={
+                                                project.demo ?? project.source
+                                            }
+                                            className="group/link flex w-fit items-center gap-1 font-medium hover:underline"
+                                            target="_blank"
+                                        >
+                                            {project.demo
+                                                ? "View Demo"
+                                                : "View Source"}
+                                            <Icons.chevronRight className="size-0 transition-all ease-in-out group-hover/link:size-5" />
+                                        </Link>
+                                        <p className="text-background/60">
+                                            {project.description}
+                                        </p>
+                                    </div>
+                                    <h4 className="text-xl">{project.name}</h4>
                                 </div>
-
-                                <h4 className="text-xl">{project.name}</h4>
                             </div>
-                        </div>
-                    ))}
+                        ))}
                 </div>
 
                 <h3 className="flex text-center text-4xl uppercase text-background selection:bg-white selection:text-foreground md:flex-col md:gap-4 md:text-7xl">
